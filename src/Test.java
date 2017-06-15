c6fb8d7061305c5125e503a76222bb59ef1e843b
swatichandel82 <swati.chandel82@gmail.com>
2017-06-15 22:37:46 +0200

    ab
    ab

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+try {
+	return;
+}
+finally
+{
+	int n = 10;
+	System.out.print(n);
+}
+	}
+
+}
